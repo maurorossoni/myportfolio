cfe74120846cb85b38b05d14f13db9cc9438c11a
maurorossoni <86635091+maurorossoni@users.noreply.github.com>
2022-02-09 16:38:02 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+
+//SERVIÇO DO EMAIL
+
+
+window.addEventListener("DOMContentLoaded", function () {
+    // get the form elements defined in your form HTML above
+  
+    var form = document.getElementById("my-form");
+    // var button = document.getElementById("my-form-button");
+    var status = document.getElementById("status");
+  
+    // Success and Error functions for after the form is submitted
+  
+    function success() {
+      form.reset();
+      status.classList.add("success");
+      status.innerHTML = "Seu e-mail foi enviado com sucesso =)";
+    }
+  
+    function error() {
+      status.classList.add("error");
+      status.innerHTML = "Opa! faltou alguma informação. =(";
+    }
+  
+    // handle the form submission event
+  
+    form.addEventListener("submit", function (ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+  
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
--- carosel.js
+++ carosel.js
@@ -1,54 +1,53 @@
 document.querySelector("#items").addEventListener("wheel", (event) => {
-    if (event.deltaY > 0) {
-      event.target.scrollBy(300, 0)
-    } else {
-      event.target.scrollBy(-300, 0)
-    }
-  })
-  
-  function insertButtons(upperDiv) {
-    for (let i = 0; i < qtBanners; i++) {
-      let a = document.createElement("a")
-      a.addEventListener("click", (event) => buttonClicked(event.target, i))
-      a.innerText = i
-      upperDiv.appendChild(a)
-    }
-  
-    navigationButtons.children[0].classList.add("selectedItem")
+  if (event.deltaY > 0) {
+    event.target.scrollBy(300, 0)
+  } else {
+    event.target.scrollBy(-300, 0)
   }
-  
-  function buttonClicked(btn, item) {
-    clearInterval(intervalId)
-    nextBanner(btn, item)
-    intervalId = autoPlay()
+})
+
+function insertButtons(upperDiv) {
+  for (let i = 0; i < qtBanners; i++) {
+    let a = document.createElement("a")
+    a.addEventListener("click", (event) => buttonClicked(event.target, i))
+    a.innerText = i
+    upperDiv.appendChild(a)
   }
-  
-  function nextBanner(btn, item) {
-    let buttons = navigationButtons.children
-    for (i = 0; i < buttons.length; i++) {
-      buttons[i].classList.remove("selectedItem")
-    }
-    btn.classList.add("selectedItem")
-    selectedItem = item
-    bannersTop.scrollTo(bannersTop.offsetWidth * selectedItem, 0)
+
+  navigationButtons.children[0].classList.add("selectedItem")
+}
+
+function buttonClicked(btn, item) {
+  clearInterval(intervalId)
+  nextBanner(btn, item)
+  intervalId = autoPlay()
+}
+
+function nextBanner(btn, item) {
+  let buttons = navigationButtons.children
+  for (i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("selectedItem")
   }
-  
-  function autoPlay() {
-    return setInterval(() => {
-      if (selectedItem < qtBanners - 1) selectedItem++
-      else selectedItem = 0
-      nextBanner(navigationButtons.children[selectedItem], selectedItem)
-    }, 3000)
-  }
-  
-  const bannersTop = document.getElementById("items")
-  var selectedItem = 0
-  const qtBanners = bannersTop.children.length
-  
-  const navigationButtons = document.createElement("div")
-  navigationButtons.id = "navigationButtons"
-  document.getElementById("items-wrapper").appendChild(navigationButtons)
-  insertButtons(navigationButtons)
-  
-  var intervalId = autoPlay()
-  
+  btn.classList.add("selectedItem")
+  selectedItem = item
+  bannersTop.scrollTo(bannersTop.offsetWidth * selectedItem, 0)
+}
+
+function autoPlay() {
+  return setInterval(() => {
+    if (selectedItem < qtBanners - 1) selectedItem++
+    else selectedItem = 0
+    nextBanner(navigationButtons.children[selectedItem], selectedItem)
+  }, 3000)
+}
+
+const bannersTop = document.getElementById("items")
+var selectedItem = 0
+const qtBanners = bannersTop.children.length
+
+const navigationButtons = document.createElement("div")
+navigationButtons.id = "navigationButtons"
+document.getElementById("items-wrapper").appendChild(navigationButtons)
+insertButtons(navigationButtons)
+
+var intervalId = autoPlay()
