c88b552d79c7a318f728f4ee2f3d4330ec5c509d
maurorossoni <86635091+maurorossoni@users.noreply.github.com>
2022-02-09 21:11:39 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,72 +4,72 @@ const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a'
 const header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
 });
 
 document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = '#29323c';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
 });
 
 menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
 });
 
 
 //SERVIÇO DO EMAIL
 
 
-window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("DOMContentLoaded", function() {
     // get the form elements defined in your form HTML above
-  
+
     var form = document.getElementById("my-form");
     // var button = document.getElementById("my-form-button");
     var status = document.getElementById("status");
-  
+
     // Success and Error functions for after the form is submitted
-  
+
     function success() {
-      form.reset();
-      status.classList.add("success");
-      status.innerHTML = "Seu e-mail foi enviado com sucesso =)";
+        form.reset();
+        status.classList.add("success");
+        status.innerHTML = "Seu e-mail foi enviado com sucesso =)";
     }
-  
+
     function error() {
-      status.classList.add("error");
-      status.innerHTML = "Opa! faltou alguma informação. =(";
+        status.classList.add("error");
+        status.innerHTML = "Oppss! faltou alguma informação. =(";
     }
-  
+
     // handle the form submission event
-  
-    form.addEventListener("submit", function (ev) {
-      ev.preventDefault();
-      var data = new FormData(form);
-      ajax(form.method, form.action, data, success, error);
+
+    form.addEventListener("submit", function(ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
     });
-  });
-  
-  // helper function for sending an AJAX request
-  
-  function ajax(method, url, data, success, error) {
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
     var xhr = new XMLHttpRequest();
     xhr.open(method, url);
     xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState !== XMLHttpRequest.DONE) return;
-      if (xhr.status === 200) {
-        success(xhr.response, xhr.responseType);
-      } else {
-        error(xhr.status, xhr.response, xhr.responseType);
-      }
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
     };
     xhr.send(data);
-  }
+}
